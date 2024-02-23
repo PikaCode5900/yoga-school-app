992c20567f6a28a5d347fa039ce948c1334b5f7e
PikaCode5900 <pableitor5900@gmail.com>
2024-02-23 10:45:08 +0100

    Se añaden los apartados de la web, se prepara su diseño y rutas

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,48 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Narayana Yoga | Marían Rivas' });
+  res.render('home-page', { title: 'Narayana Yoga | Pagina Principal' });
 });
+
+/* GET teachings page. */
+router.get('/teachings', function(req, res, next) {
+  res.render('teachings', { title: 'Narayana Yoga | Enseñanzas' });
+});
+
+/* GET training page. */
+router.get('/training', function(req, res, next) {
+  res.render('training', { title: 'Narayana Yoga | FORMACIÓN MFY' });
+});
+
+/* GET tools page. */
+router.get('/tools', function(req, res, next) {
+  res.render('tools', { title: 'Narayana Yoga | Herramientas' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Narayana Yoga | SOBRE MI' });
+});
+
+/* GET practices page. */
+router.get('/practices', function(req, res, next) {
+  res.render('practices', { title: 'Narayana Yoga | PRÁCTICAS' });
+});
+
+/* GET insights page. */
+router.get('/insights', function(req, res, next) {
+  res.render('insights', { title: 'Narayana Yoga | INSIGHTS' });
+});
+
+/* GET mentoring page. */
+router.get('/mentoring', function(req, res, next) {
+  res.render('mentoring', { title: 'Narayana Yoga | Mentorías y consultas' });
+});
+
+/* GET comments page. */
+router.get('/comments', function(req, res, next) {
+  res.render('comments', { title: 'Narayana Yoga | Comentarios destacados' });
+});
+
 
 module.exports = router;
